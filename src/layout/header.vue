9cf12db69d62b662bfa45ed1804051d6c4e4ccac
LCQQQQ <2286552044@qq.com>
2021-01-03 14:32:39 +0800

    暂存更改

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,6 +1,30 @@
 <template>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="240px">
+      <el-row class="tac">
+        <el-col>
+          <h5>后台管理系统</h5>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+            <el-submenu index="1">
+              <template #title>
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template #title>分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-aside>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
@@ -17,34 +41,24 @@ export default {
   created() {},
   mounted() {},
   computed: {},
-  methods: {}
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-  height: 100vh;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
+.el-container {
+  .el-aside {
+    background-color: deepskyblue;
+    height: 100vh;
+  }
+  .el-header {
+    background-color: greenyellow;
+  }
 }
 </style>
